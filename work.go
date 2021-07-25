4dd97a5d6b4dfa9d43c45c59920513663ec8afe0
plume <1750132640@qq.com>
2021-07-25 16:49:52 +0800

    add work.go

--- work.go
+++ work.go
@@ -1,55 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `
-<html>
-  <form action="/multipartform?lang=cpp&name=dj" method="post" enctype="multipart/form-data">
-	  <label>MultipartForm:</label>
-      <input type="text" name="lang" />
-      <input type="text" name="age" />
-      <input type="file" name="uploaded" />
-    <button type="submit">提交</button>
-  </form>
-</html>
-`)
-}
-
-func multipartFormHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fmt.Fprintln(w, r.MultipartForm)
-
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
-	if err != nil {
-		fmt.Println("Open failed: ", err)
-		return
-	}
-
-	data, err := ioutil.ReadAll(file)
-	if err == nil {
-		fmt.Fprintln(w, string(data))
-	}
-}
-
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", handle)
-	mux.HandleFunc("/multipartform", multipartFormHandler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	r := gin.Default()
+	r.LoadHTMLFiles("html/index.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", "")
+	})
+	r.POST("/form", func(c *gin.Context) {
+		user := c.PostForm("user")
+		c.String(200, user)
+		password := c.PostForm("password")
+		c.String(200, password)
+	})
+	r.Run(":8080")
 }
